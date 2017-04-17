375c8cc856e75ba9213045e28c3ccdc89ad336e3
Cam Campbell <camjcampbell@yahoo.com>
2017-04-17 15:54:00 -0400

    Update search.rkt
    Added additional video information (channel uploader)
    Fixed issues with best result procedure.

--- search.rkt
+++ search.rkt
@@ -10,7 +10,9 @@
 
   (define findVideo #px"data-video-ids=\"[a-zA-Z0-9\\-_]*")
   (define getTitle #px"\"  title=\"[a-zA-Z 0-9.,<>:;~|()!@#$%^&*+=\\-\\[\\]_'/]*")
-  (define getVerified #px"\" >[a-zA-Z0-9!@/\\%\\^\\#\\&)(\\-_., ]*</a>[\\&\\<]")
+  (define getUploader #px"\" >[a-zA-Z0-9!@/\\%\\^\\#\\&)(\\-_., ]*")
+  (define getVerified #px"</a>[\\&\\<]")
+  ;(define getVerified #px"\" >[a-zA-Z0-9!@/\\%\\^\\#\\&)(\\-_., ]*</a>[\\&\\<]")
 
   (define (loop lst n)
     (if (= n 0) (bestResult (reverse lst))
@@ -20,23 +22,25 @@
                 (cleanRegexp (regexp-match findVideo myport) #px"data-video-ids=\"")
                 ;; get video Title
                 (cleanRegexp (regexp-match getTitle myport) #px"\"  title=\"")
+                ;; get video Uploader
+                (cleanRegexp (regexp-match getUploader myport) #px"\" >")
                 ;; is video uploaded by Verified user? (#t or #f)
                 (string=?
-                     (cleanRegexp (regexp-match getVerified myport)
-                                  #px"\" >[a-zA-Z0-9!@/\\%\\^\\#\\&)(\\-_., ]*</a>")
-                     "&")) ;; if this string = '&' the uploader is Verified
+                     (cleanRegexp (regexp-match getVerified myport) #px"</a>")
+                     "&");; if this string = '&' the uploader is Verified
+                ) 
                lst) (sub1 n))))
   (loop '() 3))
 
-;sorts through list of results and finds best video
+;sorts through list of results and finds best video, returns list with video info
 (define (bestResult lst)
   (define (iter videos first)
     (cond
       [(null? videos) first] ;final case, return first video result
-      [(caddr (car videos)) (caar videos)] ;if video is verified
-      [(string-contains? (string-downcase (cadr (car videos))) "vevo") (caar videos)] ;if it is uploaded by VEVO
+      [(cadddr (car videos)) (car videos)] ;if video is verified
+      [(string-contains? (string-downcase (caddr (car videos))) "vevo") (car videos)] ;if it is uploaded by VEVO
       [else (iter (cdr videos) first)])) ; recurse
-  (iter lst (caar lst))) ; save first video result
+  (iter lst (car lst))) ; save first video result
 
 ;; clean up the regular expression and convert to string
 (define (cleanRegexp match cleanStr)
